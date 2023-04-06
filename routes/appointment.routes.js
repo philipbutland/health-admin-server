75f7cf6114c953cbc843c07df78c114dfbe7d6fd
tiagofaraujo <tiago_araujo@hotmail.com>
2023-04-06 19:08:20 +0100

    solved admin issues in server

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -12,7 +12,7 @@ router.post('/appointments/add-appointment', (req,res,next)=> {
     .then(newAppointment=>{
         globalAppointment=newAppointment
         //verify this code!
-        return Patient.findByIdAndUpdate({username: patientId},{$push:{appointment:globalAppointment._id}})
+        return Patient.findByIdAndUpdate( patientId,{$push:{appointment:globalAppointment._id}})
     })
     .then(() => {
       res.json(globalAppointment);
@@ -56,14 +56,14 @@ router.put("/appointments/:appointmentId", (req,res,next) => {
         .catch(error=>res.status(400).json( {message: error}))
 });
 
-router.get("/appointments/:patientId", (req, res, next) => {
+router.get("/appointments/patients/:patientId", (req, res, next) => {
   const { patientId } = req.params;
   console.log(patientId)
   console.log("GET");
   Appointment.find({ patientId })
-    .then((allAppointments) => {
-      console.log(allAppointments)
-      res.json(allAppointments);
+    .then((AppointmentsId) => {
+      console.log(AppointmentsId)
+      res.json(AppointmentsId);
     })
     .catch((error) => console.log(error));
 });
